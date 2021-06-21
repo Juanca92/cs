11e4b665ed3bbfe7a5be9e06103e7fa32e3b17f2
juanzapanacondori@gmail.com <juanzapanacondori@gmail.com>
2021-06-20 22:40:39 -0400

    Agregado listado de roles en administracion

---
+++ app/Views/roles/index.php
@@ -0,0 +1,47 @@
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1></h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/home">Inicio</a></li>
+                    <li class="breadcrumb-item active">Roles</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-title">
+                            Roles
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <table id="tbl_roles" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th>Grupo</th>
+                                    <th>Estado</th>
+                                    <th>Registrado</th>
+                                </tr>
+                            </thead>
+                        </table>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /.content -->
