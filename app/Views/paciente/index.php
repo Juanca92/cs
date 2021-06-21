693d612d6bab12ef1d03bce49082204f2661b6bf
juanzapanacondori@gmail.com <juanzapanacondori@gmail.com>
2021-06-20 22:18:07 -0400

    Ruta cambio de vistas de Inicio a /home

--- app/Views/paciente/index.php
+++ app/Views/paciente/index.php
@@ -7,7 +7,7 @@
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                    <li class="breadcrumb-item"><a href="/home">Inicio</a></li>
                     <li class="breadcrumb-item active">Pacientes</li>
                 </ol>
             </div>
